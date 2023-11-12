0eb5d94af65498a43fae7ec1c9d70fe401f91775
Viviane Le <letranviviane@gmail.com>
2023-11-12 09:17:46 -1000

    Fixed auth/sign up

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -24,6 +24,6 @@ class SignUpForm(FlaskForm):
     username = StringField(
         'username', validators=[DataRequired(), username_exists])
     email = StringField('email', validators=[DataRequired(), user_exists])
-    password = StringField('password', validators=[DataRequired()])
     first_name = StringField('first_name', validators=[DataRequired()])
     last_name = StringField('last_name', validators=[DataRequired()])
+    password = StringField('password', validators=[DataRequired()])
