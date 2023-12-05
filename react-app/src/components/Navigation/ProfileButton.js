d562037796ac730bcc91014d1f0a6fde527fd73f
Viviane Le <letranviviane@gmail.com>
2023-12-04 16:58:27 -0800

    Styled nav bar, login & signup form

--- react-app/src/components/Navigation/ProfileButton.js
+++ react-app/src/components/Navigation/ProfileButton.js
@@ -1,11 +1,10 @@
 import React, { useState, useEffect, useRef } from "react";
 import { useDispatch } from "react-redux";
-import { useHistory, NavLink } from "react-router-dom"
+import { useHistory, NavLink } from "react-router-dom";
 import { logout } from "../../store/session";
 import OpenModalButton from "../OpenModalButton";
 import LoginFormModal from "../LoginFormModal";
 import SignupFormModal from "../SignupFormModal";
-import ManagePhotocard from "../ManagePhotocard";
 import './Navigation.css';
 
 function ProfileButton({ user }) {
@@ -41,28 +40,31 @@ function ProfileButton({ user }) {
   };
 
   const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
-  // const ulClassName = "profile-dropdown" + (showMenu ? " visible" : "");
 
   const closeMenu = () => setShowMenu(false);
 
   return (
     <div className='login-container'>
-      <button onClick={openMenu}>
-        <i className="fas fa-user-circle" />
-      </button>
+      <div className='profile-icons-container' onClick={openMenu}>
+        <div className='rounded-icon-container'>
+          <img src='/images/bars.png' alt='Bars Icon' className='bars-icon' />
+          <i className="fas fa-user-circle" />
+        </div>
+      </div>
       <ul className={ulClassName} ref={ulRef}>
         {user ? (
           <div className='greeting-box'>
             <li>Hello {user.first_name}</li>
             <li>{user.email}</li>
-            <li className='manage-photocard-profile'><NavLink className='manage-line' to='/photocards/current'>
-              Manage Photocards
-            </NavLink></li>
+            <li className='manage-photocard-profile'>
+              <NavLink className='manage-line' to='/photocards/current'>
+                Manage Photocards
+              </NavLink>
+            </li>
             <li>
               <button onClick={handleLogout}>Log Out</button>
             </li>
           </div>
-
         ) : (
           <div className='login-signup-container'>
             <OpenModalButton className='login-button'
@@ -70,7 +72,6 @@ function ProfileButton({ user }) {
               onItemClick={closeMenu}
               modalComponent={<LoginFormModal />}
             />
-
             <OpenModalButton className='signup-button'
               buttonText="Sign Up"
               onItemClick={closeMenu}
--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -13,7 +13,7 @@ function Navigation({ isLoaded }){
 		<NavLink className='nav-link' exact to='/'>
 		  <div id='logo-name-container'>
 		  <i class="fa fa-crown"></i>
-			<h1 className='main-page-title'>PHOTOCA</h1>
+		  <h1 className='main-page-title'>PHOTOCA</h1>
 		  </div>
 		</NavLink>
 		</div>
@@ -21,8 +21,6 @@ function Navigation({ isLoaded }){
       {isLoaded && (
         <div id='nav-container'>
 		  <div id='login-button-container'>
-		  <i class="fa-solid fa-bars"></i>
-		  <i class="fa-solid fa-user"></i>
           <ProfileButton user={sessionUser} />
 		  </div>
         </div>
@@ -32,41 +30,3 @@ function Navigation({ isLoaded }){
 }
 
 export default Navigation;
-
-
-// import React from 'react';
-// import { useHistory } from 'react-router-dom';
-// import { useSelector } from 'react-redux';
-// import ProfileButton from './ProfileButton';
-// import './Navigation.css';
-
-// function Navigation({ isLoaded }) {
-//   const history = useHistory();
-//   const sessionUser = useSelector(state => state.session.user);
-
-//   const handlePhotocaClick = () => {
-//     history.push('/');
-//   };
-
-//   return (
-//     <div>
-//       <div id='logo-container'>
-//         <div className='nav-link' id='logo-name-container' onClick={handlePhotocaClick}>
-//           <i className="fa fa-crown"></i>
-//           <h1 className='main-page-title'>PHOTOCA</h1>
-//         </div>
-//       </div>
-
-//       {isLoaded && (
-//         <div id='nav-container'>
-//           <div id='login-button-container'>
-//             <i className="fa-solid fa-bars"></i>
-//             <ProfileButton user={sessionUser} />
-//           </div>
-//         </div>
-//       )}
-//     </div>
-//   );
-// }
-
-// export default Navigation;
